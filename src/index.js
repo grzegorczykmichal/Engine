ffc7b17bde312e361312efd775b6c46a13aed3a3
Michał Grzegorczyk <grzegorczykmichal@users.noreply.github.com>
2019-11-27 22:05:27 +0100

    Made it smoooooth

--- src/index.js
+++ src/index.js
@@ -61,10 +61,9 @@ const makeRenderer = canvas => {
     far: 1000
   });
 
-  const startTime = new Date().getMilliseconds();
-  let theta = 0;
+  const startTime = new Date().getTime();
+
   return cb => {
-    theta += 1 * (new Date().getMilliseconds() - startTime);
     ctx.save();
     ctx.clearRect(0, 0, WIDTH, HEIGHT); // clear canvas
     cb(ctx, projMatrix, WIDTH, HEIGHT, theta);
@@ -136,76 +135,83 @@ function drawTriangle(ctx, x1, y1, x2, y2, x3, y3) {
 const canvas = document.querySelector("canvas");
 const render = makeRenderer(canvas);
 
+const fps = 60;
+let theta = 0;
+let offset = 0.02;
 function repeatOften() {
-  render((ctx, projMatrix, screenWidth, screenHeight, theta) => {
-    const matRotZ = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-    matRotZ[0][0] = Math.cos(theta);
-    matRotZ[0][1] = Math.sin(theta);
-    matRotZ[1][0] = -Math.sin(theta);
-    matRotZ[1][1] = Math.cos(theta);
-    matRotZ[2][2] = 1;
-    matRotZ[3][3] = 1;
+  theta += offset;
+  document.querySelector("p").innerText = theta;
+  setTimeout(function() {
+    render((ctx, projMatrix, screenWidth, screenHeight, theta) => {
+      const matRotZ = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+      matRotZ[0][0] = Math.cos(theta);
+      matRotZ[0][1] = Math.sin(theta);
+      matRotZ[1][0] = -Math.sin(theta);
+      matRotZ[1][1] = Math.cos(theta);
+      matRotZ[2][2] = 1;
+      matRotZ[3][3] = 1;
 
-    const matRotX = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-    matRotX[0][0] = 1;
-    matRotX[1][1] = Math.cos(theta * 0.5);
-    matRotX[1][2] = Math.sin(theta * 0.5);
-    matRotX[2][1] = -Math.sin(theta * 0.5);
-    matRotX[2][2] = Math.cos(theta * 0.5);
-    matRotX[3][3] = 1;
+      const matRotX = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+      matRotX[0][0] = 1;
+      matRotX[1][1] = Math.cos(theta * 0.5);
+      matRotX[1][2] = Math.sin(theta * 0.5);
+      matRotX[2][1] = -Math.sin(theta * 0.5);
+      matRotX[2][2] = Math.cos(theta * 0.5);
+      matRotX[3][3] = 1;
 
-    for (const tri of qube.triangles) {
-      var newRotatedZ = Triangle.FromArrays(
-        multiply(tri.vectors[0].toArray(), matRotZ),
-        multiply(tri.vectors[1].toArray(), matRotZ),
-        multiply(tri.vectors[2].toArray(), matRotZ)
-      );
+      for (const tri of qube.triangles) {
+        var newRotatedZ = Triangle.FromArrays(
+          multiply(tri.vectors[0].toArray(), matRotZ),
+          multiply(tri.vectors[1].toArray(), matRotZ),
+          multiply(tri.vectors[2].toArray(), matRotZ)
+        );
 
-      var newRotatedX = Triangle.FromArrays(
-        multiply(newRotatedZ.vectors[0].toArray(), matRotX),
-        multiply(newRotatedZ.vectors[1].toArray(), matRotX),
-        multiply(newRotatedZ.vectors[2].toArray(), matRotX)
-      );
+        var newRotatedX = Triangle.FromArrays(
+          multiply(newRotatedZ.vectors[0].toArray(), matRotX),
+          multiply(newRotatedZ.vectors[1].toArray(), matRotX),
+          multiply(newRotatedZ.vectors[2].toArray(), matRotX)
+        );
 
-      const triTranslated = Triangle.From(newRotatedX);
+        const triTranslated = Triangle.From(newRotatedX);
 
-      triTranslated.vectors[0].z = newRotatedX.vectors[0].z + 3;
-      triTranslated.vectors[1].z = newRotatedX.vectors[1].z + 3;
-      triTranslated.vectors[2].z = newRotatedX.vectors[2].z + 3;
+        triTranslated.vectors[0].z = newRotatedX.vectors[0].z + 3;
+        triTranslated.vectors[1].z = newRotatedX.vectors[1].z + 3;
+        triTranslated.vectors[2].z = newRotatedX.vectors[2].z + 3;
 
-      var newtri = Triangle.FromArrays(
-        multiply(triTranslated.vectors[0].toArray(), projMatrix),
-        multiply(triTranslated.vectors[1].toArray(), projMatrix),
-        multiply(triTranslated.vectors[2].toArray(), projMatrix)
-      );
+        var newtri = Triangle.FromArrays(
+          multiply(triTranslated.vectors[0].toArray(), projMatrix),
+          multiply(triTranslated.vectors[1].toArray(), projMatrix),
+          multiply(triTranslated.vectors[2].toArray(), projMatrix)
+        );
 
-      newtri.vectors[0].x += 1;
-      newtri.vectors[1].x += 1;
-      newtri.vectors[2].x += 1;
+        newtri.vectors[0].x += 1;
+        newtri.vectors[1].x += 1;
+        newtri.vectors[2].x += 1;
 
-      newtri.vectors[0].y += 1;
-      newtri.vectors[1].y += 1;
-      newtri.vectors[2].y += 1;
+        newtri.vectors[0].y += 1;
+        newtri.vectors[1].y += 1;
+        newtri.vectors[2].y += 1;
 
-      newtri.vectors[0].x *= 0.5 * screenWidth;
-      newtri.vectors[1].x *= 0.5 * screenWidth;
-      newtri.vectors[2].x *= 0.5 * screenWidth;
+        newtri.vectors[0].x *= 0.5 * screenWidth;
+        newtri.vectors[1].x *= 0.5 * screenWidth;
+        newtri.vectors[2].x *= 0.5 * screenWidth;
 
-      newtri.vectors[0].y *= 0.5 * screenHeight;
-      newtri.vectors[1].y *= 0.5 * screenHeight;
-      newtri.vectors[2].y *= 0.5 * screenHeight;
+        newtri.vectors[0].y *= 0.5 * screenHeight;
+        newtri.vectors[1].y *= 0.5 * screenHeight;
+        newtri.vectors[2].y *= 0.5 * screenHeight;
 
-      drawTriangle(
-        ctx,
-        newtri.vectors[0].x,
-        newtri.vectors[0].y,
-        newtri.vectors[1].x,
-        newtri.vectors[1].y,
-        newtri.vectors[2].x,
-        newtri.vectors[2].y
-      );
-    }
-  });
-  requestAnimationFrame(repeatOften);
+        drawTriangle(
+          ctx,
+          newtri.vectors[0].x,
+          newtri.vectors[0].y,
+          newtri.vectors[1].x,
+          newtri.vectors[1].y,
+          newtri.vectors[2].x,
+          newtri.vectors[2].y
+        );
+      }
+    });
+    requestAnimationFrame(repeatOften);
+  }, 1000 / fps);
 }
 requestAnimationFrame(repeatOften);
